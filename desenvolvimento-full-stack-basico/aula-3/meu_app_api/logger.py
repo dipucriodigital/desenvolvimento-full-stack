dbb9b0f3ecf804ff7b35aba6f7de90ff792cf655
Fernando Correia <fernando.correia.jr@gmail.com>
2023-03-05 19:06:28 -0300

    Criacao dos direitorios de log e db caso nao existam

--- desenvolvimento-full-stack-basico/aula-3/meu_app_api/logger.py
+++ desenvolvimento-full-stack-basico/aula-3/meu_app_api/logger.py
@@ -1,5 +1,14 @@
-import logging
 from logging.config import dictConfig
+import logging
+import os
+
+
+log_path = "log/"
+# Verifica se o diretorio para armexanar os logs não existe
+if not os.path.exists(log_path):
+   # então cria o diretorio
+   os.makedirs(log_path)
+
 
 dictConfig({
     "version": 1,
--- desenvolvimento-full-stack-basico/aula-3/meu_app_api/model/__init__.py
+++ desenvolvimento-full-stack-basico/aula-3/meu_app_api/model/__init__.py
@@ -1,15 +1,21 @@
 from sqlalchemy_utils import database_exists, create_database
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+import os
 
 # importando os elementos definidos no modelo
 from model.base import Base
 from model.comentario import Comentario
 from model.produto import Produto
 
+db_path = "database/"
+# Verifica se o diretorio não existe
+if not os.path.exists(db_path):
+   # então cria o diretorio
+   os.makedirs(db_path)
 
 # url de acesso ao banco (essa é uma url de acesso ao sqlite local)
-db_url = 'sqlite:///database/db.sqlite3'
+db_url = 'sqlite:///%s/db.sqlite3' % db_path
 
 # cria a engine de conexão com o banco
 engine = create_engine(db_url, echo=False)
